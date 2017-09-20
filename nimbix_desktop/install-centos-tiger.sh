92cf37796c64d2a0dbfbfee46856e80e17330c9a
Leo Reiter <leo.reiter@nimbix.net>
2017-09-20 16:21:01 -0500

    uprev to VGL 2.5.2

--- nimbix_desktop/install-centos-tiger.sh
+++ nimbix_desktop/install-centos-tiger.sh
@@ -11,7 +11,7 @@ TIGERSRC="https://github.com/TigerVNC/tigervnc/archive/v$VERSION.tar.gz"
 XORGVER=1.17.4
 XORGSRC="https://www.x.org/archive//individual/xserver/xorg-server-$XORGVER.tar.gz"
 
-VGL64VER=2.5
+VGL64VER=2.5.2
 VGL64="https://downloads.sourceforge.net/project/virtualgl/$VGL64VER/VirtualGL-${VGL64VER}.x86_64.rpm"
 VGL32="https://downloads.sourceforge.net/project/virtualgl/$VGL64VER/VirtualGL-${VGL64VER}.i386.rpm"
 VGL64SRC="https://downloads.sourceforge.net/project/virtualgl/$VGL64VER/VirtualGL-$VGL64VER.tar.gz"
