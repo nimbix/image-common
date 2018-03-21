7cc26481844718a94d47003ee4fc6f6703e58536
Paul Schroeder <paul.schroeder@nimbix.net>
2018-03-20 21:32:50 -0500

    Use update in case already installed..

--- nimbix_desktop/install-centos-real.sh
+++ nimbix_desktop/install-centos-real.sh
@@ -8,7 +8,7 @@ REALVNC_VER=6.2.1
 REALVNC="https://www.realvnc.com/download/file/vnc.files/VNC-Server-$REALVNC_VER-Linux-x64.rpm"
 wget --content-disposition -O /tmp/$(basename $REALVNC) "$REALVNC"
 rm -f /usr/bin/Xvnc
-yum -y install /tmp/VNC-Server-*-Linux-x64.rpm
+yum -y update /tmp/VNC-Server-*-Linux-x64.rpm
 rm -f /tmp/VNC-*.rpm
 [ -e /usr/bin/Xvnc ] || ln -sf /usr/bin/Xvnc-realvnc /usr/bin/Xvnc
 
