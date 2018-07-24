c320b956cbc09da1f8e451f2523d60723a930f79
jhegge <johnathan.hegge@nimbix.net>
2018-07-24 13:25:23 -0500

    install xterm first, remove some manual steps that now are part of vnc server

--- nimbix_desktop/install-centos-real.sh
+++ nimbix_desktop/install-centos-real.sh
@@ -19,18 +19,18 @@ VERSION_ID=$(cat /etc/system-release-cpe | awk -F: '{print $5}')
 yum -y groupinstall Xfce
 yum -y groupinstall Fonts
 yum -y install perl wget xauth pygtk2 gnome-icon-theme xorg-x11-fonts-Type1 xorg-x11-fonts-misc xorg-x11-fonts-75dpi xorg-x11-fonts-100dpi xkeyboard-config firefox net-tools glx-utils xorg-x11-utils
-yum -y install xorg-x11-fonts-ISO8859-1-100dpi xorg-x11-fonts-ISO8859-1-75dpi compat-libstdc++-33 python-pip ImageMagick-devel xorg-x11-apps
+yum -y install xorg-x11-fonts-ISO8859-1-100dpi xorg-x11-fonts-ISO8859-1-75dpi compat-libstdc++-33 python-pip ImageMagick-devel xorg-x11-apps xterm
 if [ ${VERSION_ID} -gt 6 ]; then
     yum -y install ristretto
 fi
 
-curl $REALVNC >/tmp/$(basename $REALVNC)
+curl ${REALVNC} >/tmp/$(basename $REALVNC)
 #wget --content-disposition -O - "$REALVNC"|tar -C /tmp -xzf -
-rm -f /usr/bin/Xvnc
+#rm -f /usr/bin/Xvnc   # done by VNC install
 yum -y install /tmp/VNC-Server-*-Linux-x64.rpm || \
     yum -y update /tmp/VNC-Server-*-Linux-x64.rpm
 rm -f /tmp/VNC-*.rpm
-ln -sf /usr/bin/Xvnc-realvnc /usr/bin/Xvnc
+#ln -sf /usr/bin/Xvnc-realvnc /usr/bin/Xvnc  # unneeded?
 
 if [ "$ARCH" != "x86_64" ]; then
     yum -y install VirtualGL
