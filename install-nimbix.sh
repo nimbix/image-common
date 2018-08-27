98d6442db9ce461dd31456646dcfddd0c3f40ac7
Leo Reiter <leo.reiter@nimbix.net>
2018-08-27 16:50:56 +0000

    use epel-release package in centos so it can better match the repos configured

--- install-nimbix.sh
+++ install-nimbix.sh
@@ -61,9 +61,7 @@ function setup_base_os() {
     PKGS="curl zip unzip sudo"
     if [ -f /etc/redhat-release ]; then
         if [ ! -f /etc/fedora-release ]; then
-            VERSION_ID=$(cat /etc/system-release-cpe | awk -F: '{print $5}')
-            EPEL="https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID}.noarch.rpm"
-            rpm -Uv $EPEL || /bin/true
+            PKGS+=" epel-release"
         fi
         PKGS+=" passwd xz tar file openssh-server infiniband-diags"
         PKGS+=" openmpi perftest libibverbs-utils libmthca libcxgb4 libmlx4"
