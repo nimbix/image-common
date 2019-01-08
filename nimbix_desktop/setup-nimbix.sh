092f415b6d8662e1bd35785fdc19445035f2e797
Kenneth Hill <ken.hill@nimbix.net>
2019-01-08 12:39:45 -0500

    Add environment variable to set user's group name. Use sudo file for nimbix user for in-container identity if exist
    Signed-off-by: Kenneth Hill <ken.hill@nimbix.net>

--- nimbix_desktop/setup-nimbix.sh
+++ nimbix_desktop/setup-nimbix.sh
@@ -1,14 +1,33 @@
 #!/bin/sh
-/usr/sbin/groupadd -g ${JARVICE_ID_GID} ${JARVICE_ID_USER}
-/usr/sbin/useradd -u ${JARVICE_ID_UID} -g ${JARVICE_ID_GID} -m -s /bin/bash \
-        ${JARVICE_ID_USER}
-cat <<EOF >/etc/sudoers.d/00-${JARVICE_ID_USER}
+# Assign default JARVICE user if not set
+# username: nimbix 
+# group: nimbix
+# uid/gid: 505/505
+JARVICE_ID_USER=${JARVICE_ID_USER:-nimbix}
+JARVICE_ID_GROUP=${JARVICE_ID_GROUP:-nimbix}
+JARVICE_ID_UID=${JARVICE_ID_UID:-505}
+JARVICE_ID_GID=${JARVICE_ID_GID:-505}
+# Create sudo file
+mkdir -p /etc/sudoers.d
+if [ -e /etc/sudoers.d/00-nimbix ]; then
+    mv /etc/sudoers.d/00-nimbix /etc/sudoers.d/00-${JARVICE_ID_USER}
+    sed -i "s/nimbix/${JARVICE_ID_USER}/g" /etc/sudoers.d/00-${JARVICE_ID_USER}
+    # Attempt to remove default nimbix user
+    /usr/sbin/userdel -r nimbix || true
+else
+    cat <<EOF >/etc/sudoers.d/00-${JARVICE_ID_USER}
 Defaults: ${JARVICE_ID_USER} !requiretty
 Defaults: root !requiretty
 ${JARVICE_ID_USER} ALL=(ALL) NOPASSWD: ALL
 EOF
+fi
 chmod 0440 /etc/sudoers.d/00-${JARVICE_ID_USER}
-chown ${JARVICE_ID_USER}:${JARVICE_ID_USER} /data
+# Create linux/unix user
+/usr/sbin/groupadd -g ${JARVICE_ID_GID} ${JARVICE_ID_GROUP}
+/usr/sbin/useradd -u ${JARVICE_ID_UID} -g ${JARVICE_ID_GID} -m -s /bin/bash \
+        ${JARVICE_ID_USER}
+# Setup mount point for JARVICE vault
+chown ${JARVICE_ID_USER}:${JARVICE_ID_GROUP} /data
 # recreate nimbix user home to get the right skeleton files
 /bin/rm -rf /home/${JARVICE_ID_USER}
 /sbin/mkhomedir_helper ${JARVICE_ID_USER}
