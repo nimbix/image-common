f74c704639bfce4c50fdc887b9365fa9dab5c4c9
jhegge <johnathan.hegge@nimbix.net>
2020-05-26 18:23:39 -0500

    re: #2771
    still issues with Tiger, try building

--- nimbix_desktop/install-centos-tiger.sh
+++ nimbix_desktop/install-centos-tiger.sh
@@ -6,6 +6,8 @@ ARCH=$(arch)
 # update links as needed
 TIGERVNC="https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-$VERSION.$ARCH.tar.gz"
 #TIGERSRC="https://github.com/TigerVNC/tigervnc/archive/v$VERSION.tar.gz"
+TIGERSRC="https://github.com/TigerVNC/tigervnc/archive/master.tar.gz"
+
 
 #XORGVER=1.18.4
 #XORGVER=1.17.4
@@ -43,6 +45,56 @@ else
          xcb-util xcb-util-keysyms
 fi
 
+function build_and_install_tiger() {
+    yum -y install git cmake make gcc-c++ \
+        libX11-devel zlib-devel turbojpeg-devel libjpeg-turbo-devel \
+        gnutls-devel gettext fltk-devel libXrender-devel autoconf automake \
+        libtool pkgconfig bison flex gperf m4 ncurses-devel intltool \
+        llvm libtalloc-devel mesa-libgbm-devel mtdev-devel libgcrypt-devel \
+        openssl-devel fontconfig libpng-devel freetype-devel imake \
+        libXfont-devel xorg-x11-server-devel pixman-devel xorg-x11-font-utils \
+        xorg-x11-proto-devel xorg-x11-xtrans-devel libxkbfile-devel
+
+    wget --content-disposition -O - "$TIGERSRC" |tar -C /tmp -xzf -
+    TIGERVNC_SOURCE=/tmp/tigervnc-$VERSION
+    BUILD_DIR=/tmp/tigervnc_build
+    mkdir -p $BUILD_DIR
+    cd $BUILD_DIR
+    cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_VIEWER=OFF \
+        $TIGERVNC_SOURCE
+    make
+    make install
+
+    cp -R $TIGERVNC_SOURCE/unix/xserver $BUILD_DIR/unix/
+    wget --content-disposition -O - "$XORGSRC" |tar -C /tmp -xzf -
+    cp -R /tmp/xorg-server-$XORGVER/* $BUILD_DIR/unix/xserver/
+    cd $BUILD_DIR/unix/xserver
+    #patch -p1 < "$TIGERVNC_SOURCE/unix/xserver118.patch"
+    patch -p1 < "$TIGERVNC_SOURCE/unix/xserver117.patch"
+    autoreconf -fiv
+    ./configure --prefix=/usr --with-pic --without-dtrace \
+        --disable-static --disable-dri \
+        --disable-xinerama --disable-xvfb --disable-xnest --disable-xorg \
+        --disable-dmx --disable-xwin --disable-xephyr --disable-kdrive \
+        --disable-config-hal --disable-config-udev \
+        --disable-dri2 --enable-glx \
+        --with-xkb-path=/usr/share/X11/xkb \
+        --with-xkb-output=/var/lib/xkb \
+        --with-xkb-bin-directory=/usr/bin \
+        --with-serverconfig-path=/usr/lib/xorg
+    make TIGERVNC_SRCDIR=$TIGERVNC_SOURCE install
+
+    #wget --content-disposition -O - "$VGL64SRC" |tar -C /tmp -xzf -
+    #VGL64_SOURCE=/tmp/VirtualGL-$VGL64VER
+    #BUILD_DIR=/tmp/vgl64_build
+    #mkdir -p $BUILD_DIR
+    #cd $BUILD_DIR
+    #cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=/usr \
+    #    -DTJPEG_LIBRARY="-L/usr/lib64 -lturbojpeg" $VGL64_SOURCE
+    #make
+    #make install
+}
+
 if [ $VERSION_ID -gt 6 ]; then
     yum -y install ristretto
 fi
@@ -51,8 +103,11 @@ if [ "$ARCH" != "x86_64" ]; then
     #build_and_install_tiger
     yum -y install tigervnc-server VirtualGL
 else
+    # try upstream tiger
+    build_and_install_tiger
+
     # Grab newer binary packages on x86_64
-    wget --content-disposition -O - "$TIGERVNC" |tar -C / -xzf - --strip-components=1
+#    wget --content-disposition -O - "$TIGERVNC" |tar -C / -xzf - --strip-components=1
     cd /tmp
     wget --content-disposition "$VGL64"
     wget --content-disposition "$VGL32"
