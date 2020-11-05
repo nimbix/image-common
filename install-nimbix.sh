a7fd549800e2b2c52eba01786624f143967d69c2
jhegge <johnathan.hegge@nimbix.net>
2020-11-05 13:35:06 -0600

    revert the soft link for /usr/lib/JARVICE, still requires a copy

--- install-nimbix.sh
+++ install-nimbix.sh
@@ -120,12 +120,13 @@ function setup_jarvice_emulation {
     rm -f nimbix.zip
     /tmp/image-common-$BRANCH/setup-nimbix.sh
 
-    # Redundant directory copies, use a soft link, favor the /usr/local/
-    # mkdir -p /usr/lib/JARVICE
-    # cp -a /tmp/image-common-$BRANCH/tools /usr/lib/JARVICE
+    # Redundant directory copies, use a soft link, favor the /usr/local/ but
+    #  J2 depends on this so allow the full copies for now
+     mkdir -p /usr/lib/JARVICE
+     cp -a /tmp/image-common-$BRANCH/tools /usr/lib/JARVICE
     mkdir -p /usr/local/JARVICE
     cp -a /tmp/image-common-$BRANCH/tools /usr/local/JARVICE
-    ln -sf /usr/local/JARVICE /usr/lib/JARVICE
+#    ln -sf /usr/local/JARVICE /usr/lib/JARVICE
     cat <<'EOF' | tee /etc/profile.d/jarvice-tools.sh >/dev/null
 JARVICE_TOOLS="/usr/local/JARVICE/tools"
 JARVICE_TOOLS_BIN="$JARVICE_TOOLS/bin"
