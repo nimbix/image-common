266b84c3817ab275949367d6fb13c41ab9df331f
Johnathan Hegge <johnathan.hegge@nimbix.net>
2020-12-15 22:07:59 -0700

    re: #3776
    set location

--- nimbix_desktop/prep-tiger.sh
+++ nimbix_desktop/prep-tiger.sh
@@ -7,4 +7,5 @@ ARCH=$(arch)
 TIGERVNC="https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-$VERSION.$ARCH.tar.gz"
 
 # Grab tarballs on x86_64, install in place to an location that needs pathing
+cd /usr/local/lib/nimbix_desktop
 wget --content-disposition -O - "$TIGERVNC"
