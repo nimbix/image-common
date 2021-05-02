66b70d611c8387cd30936058d56f6b82ffbcee02
Johnathan Hegge <johnathan.hegge@nimbix.net>
2021-05-02 08:18:14 -0500

    switching to 1.10.1 from cache

--- nimbix_desktop/prep-tiger.sh
+++ nimbix_desktop/prep-tiger.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-VERSION=1.9.0
+VERSION=1.10.1
 ARCH=$(arch)
 
 # update links as needed
-TIGERVNC="https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-$VERSION.$ARCH.tar.gz"
+#TIGERVNC="https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-$VERSION.$ARCH.tar.gz"
+TIGERVNC="https://storage.googleapis.com/app_archive/tigervnc/tigervnc-$VERSION.$ARCH.tar.gz"
 
 # Grab tarballs on x86_64, install in place to an location that needs pathing
 cd /usr/local/lib/nimbix_desktop
