ce6b9abb27a8e3e3d5a4b50550e4b30f380cdf5d
Johnathan Hegge <johnathan.hegge@nimbix.net>
2022-01-10 15:06:54 -0600

    JAR-4647
    drop the skel copying

--- nimbix_desktop/postinstall-desktop.sh
+++ nimbix_desktop/postinstall-desktop.sh
@@ -5,12 +5,12 @@
 #cp -r $dirname/skel.config/xfce4 /etc/skel/.config
 #chmod -R u+w /etc/skel/.config/xfce4
 
-SKEL_CONFIGS=$(cd $dirname/skel.config && find . -type f)
-for i in $SKEL_CONFIGS; do
-    mkdir -p $(dirname /etc/skel/.config/$i)
-    cp -f $dirname/skel.config/$i /etc/skel/.config/$i
-    chmod u+w /etc/skel/.config/$i
-done
+#SKEL_CONFIGS=$(cd $dirname/skel.config && find . -type f)
+#for i in $SKEL_CONFIGS; do
+#    mkdir -p $(dirname /etc/skel/.config/$i)
+#    cp -f $dirname/skel.config/$i /etc/skel/.config/$i
+#    chmod u+w /etc/skel/.config/$i
+#done
 
 # Copy in the config files to set Firefox as default
 cp -f $dirname/helpers.rc /etc/skel/.config
