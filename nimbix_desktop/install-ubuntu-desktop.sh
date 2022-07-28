3ac69b3d6e300c2077561a71020b897f13f3a580
oxedions <oxedions@gmail.com>
2022-07-28 22:06:02 +0200

    JAR-5414 - Add missing debus package

--- nimbix_desktop/install-ubuntu-desktop.sh
+++ nimbix_desktop/install-ubuntu-desktop.sh
@@ -16,7 +16,7 @@ apt-get -y install wget gnome-icon-theme software-properties-common \
     fonts-freefont-ttf xfonts-base xfonts-100dpi xfonts-75dpi x11-apps \
     xfonts-scalable xauth firefox ristretto mesa-utils init-system-helpers \
     libxcb1 libxcb-keysyms1 libxcb-util1 librtmp1 python3-numpy \
-    gir1.2-gtk-3.0 libxv1 libglu1-mesa --no-install-recommends
+    gir1.2-gtk-3.0 libxv1 libglu1-mesa dbus-x11 --no-install-recommends
 
 if [[ "$ARCH" != "x86_64" ]]; then
     echo "non-x86_64 has no VirtualGL"
